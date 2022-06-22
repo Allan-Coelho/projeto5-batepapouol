21a0b8c187890b0f833f0f999505e105b66aae39
Allan Vieira Coelho <63244077+Allan-Coelho@users.noreply.github.com>
2022-06-22 11:21:10 -0300

    Commit inicial + teste

---
+++ script.js
@@ -0,0 +1,27 @@
+let messages = [];
+const apiURL = "https://mock-api.driven.com.br/api/v6/uol/";
+
+main();
+function main() {
+  refreshMessages();
+  console.log(messages);
+}
+
+/**
+ * Essa função retorna um array com as menssagens do servidor;
+ * @returns {message[]} Array contendo objetos do tipo message;
+ * @returns {Object} message Objeto com as propriedades de uma mensagem;
+ * @returns {String} message.from Quem enviou a mensagem;
+ * @returns {String} message.to Quem é o destinatário da mensagem;
+ * @returns {String} message.text Conteúdo da mensagem;
+ * @returns {String} message.type Tipo da mensagem;
+ * @returns {String} message.time Quando a mensagem foi enviada;
+ */
+function refreshMessages() {
+  const promise = axios.get(apiURL + "messages");
+  promise.then((x) => {
+    messages = x;
+  });
+}
+
+getMessages();
