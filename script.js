c3a74d8c96db0f61da4dc516cb629ba6e88c2417
Allan Vieira Coelho <allan.coelho999@gmail.com>
2022-06-27 12:12:05 -0300

    Tipo: layout responsivo,scroll, problemas com ping

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let messages = [];
+const bodyContainer = document.querySelector(".container");
 const apiURL = "https://mock-api.driven.com.br/api/v6/uol/";
-
+/* const username = prompt('Qual o seu nome?'); */
 
 /**
  * Essa função retorna um array com as menssagens do servidor;
@@ -12,16 +13,73 @@ const apiURL = "https://mock-api.driven.com.br/api/v6/uol/";
  * @returns {String} message.type Tipo da mensagem;
  * @returns {String} message.time Quando a mensagem foi enviada;
  */
- function refreshMessages() {
-    const promise = axios.get(apiURL + "messages");
-    promise.then((x) => {
-      console.log(x)
+function refreshMessages() {
+  const promise = axios.get(apiURL + "messages");
+  messages = [];
+  promise.then((x) => {
+    x.data.forEach(function (item) {
+      newMessage(item);
     });
-  }
-
-main();
-function main() {
-  refreshMessages();
-  console.log(messages);
+  });
 }
 
+function standardMessage(message) {
+  const template = `
+  <div class="message standard">
+  <p>
+    <span class="time">(${message.time})</span>
+    <span><span class="bold">${message.from}</span> para <span class="bold">${message.to}</span>${message.text}</span>
+  </p>
+  </div>`;
+
+  bodyContainer.innerHTML += template;
+}
+
+function statusMessage(message) {
+  const template = `
+  <div class="message status">
+    <span class="time">(${message.time})</span>
+    <span><span class="bold">${message.from}</span>${message.text}</span>
+  </div>`;
+  bodyContainer.innerHTML += template;
+}
+
+function privateMessage(message) {
+  const template = `
+  <div class="message private">
+    <span class="time">(${message.time})</span>
+    <span><span class="bold">${message.from}</span>reservadamente para <span class="bold">${message.to}:</span>${message.text}</span>
+  </div>`;
+  bodyContainer.innerHTML += template;
+}
+
+function renderMessages() {
+  bodyContainer.innerHTML = "";
+  console.log(messages);
+  for (let index = 0, len = messages.length; index < len; index++) {
+    newMessage(messages[index]);
+    console.log(messages[index]);
+  }
+}
+
+function newMessage(message) {
+  if (message.type === "status") {
+    statusMessage(message);
+    return;
+  }
+  if (message.type === "message") {
+    standardMessage(message);
+    return;
+  }
+  if (message.type === "private_message") {
+    privateMessage(message);
+    return;
+  }
+}
+
+function main() {
+  refreshMessages();
+  renderMessages();
+}
+
+main();
